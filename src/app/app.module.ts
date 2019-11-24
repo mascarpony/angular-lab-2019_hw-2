78f8a92dc20fb0ae8230c6bdaa1bc2497145a0e5
Ivan Radchenko <radchenk@gmail.com>
2019-11-24 23:21:52 +0300

    Add App component dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { FooterComponent } from "./footer/footer.component";
+
+import { HttpClientModule } from "@angular/common/http";
+import { MaterialElevationDirective } from "./diractives/material-elevation/material-elevation.directive";
+
+import { CafeModule } from "./cafe/cafe.module";
+import { AppMaterialModule } from "./app-material/app-material.module";
+import { CommunicatorService } from "./cafe/services/communicator.service";
+import { ConfigService } from "./services/config/config.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    MaterialElevationDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CafeModule,
+    AppMaterialModule
   ],
-  providers: [],
+  providers: [CommunicatorService, ConfigService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
