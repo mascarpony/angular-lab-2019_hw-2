2bfabadefee1d70a7ab590247aa4baa6cf71cae0
Anatoly Shlom <a.m.shlom@gmail.com>
2019-11-26 15:06:41 +0300

    cleanup

--- Pinzeria/src/app/app.component.ts
+++ Pinzeria/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from './product';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +8,5 @@ import { Product } from './product';
 
 export class AppComponent {
   title = 'Pinzeria';
-  products = [
-    new Product('Salame and Parma', 30 , 'Сlassic and most delicious pinza'),
-    new Product('Cola', 5 , 'Legendary drink'),
-    new Product('Coffe', 5 , 'Will keep you awake'),
-    new Product('Salad', 5 , 'Always fresh')
-  ];
 }
 
